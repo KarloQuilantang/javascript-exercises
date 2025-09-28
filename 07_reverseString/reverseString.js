683ae814c307af1ffd94ec316163fd7f3d312294
KarloQuilantang <quilantangkarlo@gmail.com>
2025-09-28 18:18:13 +0800

    Finish exercise up to 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
+const reverseString = function(words) {
+const stringLength = words.length;
+text = ""
 
+for (let i = stringLength - 1; i >= 0; i--){
+    text += words[i]
+}
+return text;
 };
 
 // Do not edit below this line
